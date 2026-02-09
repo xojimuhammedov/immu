8eb04d131f5cfb4ba51956f1fb7616e962946637
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2026-02-09 22:38:47 +0500

    add

--- src/pages/MemberAbout/index.jsx
+++ src/pages/MemberAbout/index.jsx
@@ -98,7 +98,7 @@ export function TeamSection() {
     <section className="py-12 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-12 md:mb-16">
-          <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-slate-900 mb-4">
+          <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold text-slate-900 mb-4">
             {t("Islomiy moliya mutaxassislari")}
           </h2>
         </div>
